8f44574e552af2a2217e06b9f6447c07e5cfbee9
MasuqaT <occar@hotmail.co.jp>
2026-05-10 23:07:53 +0900

    test output in bun code

--- remark-bridge/index.ts
+++ remark-bridge/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import remarkParse from 'remark-parse'
+import {unified} from 'unified'
+
+const processor = unified().use(remarkParse)
+
+const value = `
+# Pluto
+
+**Pluto** (minor-planet designation: *134340 Pluto*) is a [dwarf planet](https://en.wikipedia.org/wiki/Dwarf_planet)
+in the [Kuiper belt](https://en.wikipedia.org/wiki/Kuiper_belt).
+`
+
+const ast = processor.parse(value)
+
+console.log(JSON.stringify(ast, null, 2))
