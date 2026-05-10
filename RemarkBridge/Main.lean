b87e868407d665a028938214b5d711bc62b8567f
MasuqaT <occar@hotmail.co.jp>
2026-05-10 23:49:58 +0900

    get bun version via process

--- RemarkBridge/Main.lean
+++ RemarkBridge/Main.lean
@@ -3,4 +3,10 @@ import RemarkBridge.Mdast
 open Mdast
 
 def process: IO Mdast.MdastNode := do
+  let spawnArgs: IO.Process.SpawnArgs := {
+    cmd := "bun.exe",
+    args := #["--version"],
+  }
+  let output <- IO.Process.output spawnArgs
+  dbg_trace output.stdout
   pure $ MdastNode.text "This is from mdast"
