babf06707c18c4ed95a287820b5914256b1f5270
MasuqaT <occar@hotmail.co.jp>
2026-05-16 18:16:35 +0900

    Use logger

--- Main.lean
+++ Main.lean
@@ -1,8 +1,14 @@
+import Lean
 import RemarkBridge
 import Reqlean
+import Log.Log
+
+open Lean
 
 def main : IO Unit := do
+  let logger : Logger := { minLevel := .debug }
+
   let result <- process "./README.md"
   match result with
-  | .ok result => dbg_trace result.toFormat
-  | .error e => dbg_trace e
+  | .ok result => logger.info s!"{result.toFormat}"
+  | .error e => logger.error e
